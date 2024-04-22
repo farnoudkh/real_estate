7a76f2d5e170c0e995a513b2b8660e1210b6fd91
farnoudkh <khaboshan.farnoud@gmail.com>
2024-04-22 14:55:19 +0200

    Creation of the ProperyDetail page that fetch the property

---
+++ frontend/src/components/Card.jsx
@@ -0,0 +1,38 @@
+import {Link} from 'react-router-dom'
+import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faBath} from '@fortawesome/free-solid-svg-icons'
+import {faBed} from '@fortawesome/free-solid-svg-icons'
+
+
+const Card = (props) => {
+    
+    return (
+        <div className="max-w-sm rounded overflow-hidden shadow-lg">
+            <img className="w-full h-64 object-cover" src={props.photo_main} alt="" />
+            <div className="px-6 py-4">
+                <div className="font-bold text-xl text-gray-700">{props.price}€</div>
+            </div>
+            <div className="px-6">
+                <div className="font-bold text-sm text-gray-700">{props.address} {props.zip_code} {props.city}</div>
+            </div>
+            <div className="px-6 pt-4 pb-2 items-center">
+                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                    {props.surface}m²
+                </span>
+                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                    <FontAwesomeIcon icon={faBed} className="mr-1"/>
+                    {props.bedrooms}
+                </span>
+                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                    <FontAwesomeIcon icon={faBath} className="mr-1"/>{props.bathrooms}
+                </span>
+                <span className="text-xl font-semibold text-gray-300 mr-1">|</span>            
+                <span className="text-sm font-semibold text-gray-700">{props.type}</span>
+
+            </div>
+        </div>
+    )
+};
+
+export default Card;
---
+++ frontend/src/components/Property.jsx
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from 'react';
+import AxiosInstance from './AxiosInstance';
+import Card from './Card';
+import { Link } from 'react-router-dom';
+
+const Property = () => {
+  const [properties, setProperties] = useState([]);
+
+  useEffect(() => {
+    // Fetch property data from backend
+    AxiosInstance.get('/api/listings')
+      .then(response => {
+        setProperties(response.data);
+        console.log(response)
+      })
+      .catch(error => {
+        console.error('Error fetching properties:', error);
+      });
+  }, []);
+
+  return (
+    <div className="flex flex-wrap justify-center">
+      {properties.map(property => (
+        <div key={property.slug} className="m-4 transform transition-transform duration-300 ease-in-out hover:scale-110">
+          <Link to={`/listings/${property.slug}`}>
+            <Card 
+              title={property.title} 
+              price={property.price} 
+              surface={property.surface} 
+              bathrooms={property.bathrooms} 
+              bedrooms={property.bedrooms}
+              photo_main={property.photo_main}
+              type={property.sale_type}
+              address={property.address}
+              zip_code={property.zip_code}
+              city={property.city}
+            />
+          </Link>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Property;
