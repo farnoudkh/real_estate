4a251996f2976b504e9da77a2d8f1baf99ceac20
Wolfdow <Farnoud>
2024-06-08 18:29:59 +0200

    PropertyDetail header finish

--- backend/listings/serializers.py
+++ backend/listings/serializers.py
@@ -2,11 +2,15 @@ from rest_framework import serializers
 from .models import Listing
 
 class ListingSerializer(serializers.ModelSerializer):
+    publish_date = serializers.DateField(format='%d/%m/%Y')
+
     class Meta:
         model = Listing
-        fields = ('title', 'address', 'zip_code', 'city', 'price', 'sale_type', 'home_type', 'bedrooms', 'bathrooms', 'surface', 'photo_main', 'slug')
+        fields = ('title', 'address', 'zip_code', 'city', 'price', 'sale_type', 'home_type', 'bedrooms', 'bathrooms', 'surface', 'photo_main', 'slug', 'publish_date')
 
 class listingDetailSerializer(serializers.ModelSerializer):
+    publish_date = serializers.DateField(format='%d/%m/%Y')
+
     class Meta:
         model = Listing
         fields = '__all__'
