ab920faeb888ae8a5527913806c5540d292cb75f
Wolfdow <Farnoud>
2025-07-29 19:42:15 +0200

    add base: '/realestate/'

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/realestate/',
   plugins: [react()],
   server: {
     port: 3000,
